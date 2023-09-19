0e747c03c396150054b27631bce25c66d734ef26
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-09-19 12:42:54 -0700

    fixing merge conflicts, work in progress; editing database

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
-<<<<<<< HEAD
-import AddToCart from "./AddToCart"
-=======
 import AddToCart from "./AddToCart";
->>>>>>> main
 
 function ProductDetails( ) {
 
@@ -20,23 +16,6 @@ function ProductDetails( ) {
     }, [params.productId])
 
     return(
-<<<<<<< HEAD
-        <div >
-            <NavBar/>
-            <div className="flex justify-center">
-                <div className="max-w-3xl rounded overflow-hidden shadow-lg bg-blue-200/50 m-6 center ">
-                    <div className= "flex">
-                        <img  className="w-auto h-96" src={productId.image} alt={productId.name} />
-                        <div className='px-6 py-4'>
-                            <div className="font-bold text-xl mb-2"><h1>{productId.name}</h1></div>
-                            <p class="text-gray-700 text-base"><b>Summary:</b> {productId.product_description}</p>
-                            <p className="text-gray-700 text-base bold"><b>Price: $</b> {productId.price}</p>
-                            <AddToCart />
-                        </div>
-                    </div>
-                </div>
-            </div>
-=======
         <div>
         <NavBar/>
         
@@ -51,7 +30,6 @@ function ProductDetails( ) {
             <AddToCart />
             </div>
         </div>
->>>>>>> main
         </div>
     )
 }
